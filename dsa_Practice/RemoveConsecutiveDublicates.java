56a3ec2d16c51aea74c5f3ce84ec1147ada3885d
Ankit <ankitpal28090016@gmail.com>
2022-12-09 16:23:03 +0530

    final commit

---
+++ dsa_Practice/RemoveConsecutiveDublicates.java
@@ -0,0 +1,27 @@
+package dsa_Practice;
+
+import java.util.ArrayList;
+
+public class RemoveConsecutiveDublicates {
+	public static ArrayList<Integer> removeconsecutiveDublicates(int[] arr){
+		ArrayList<Integer> result = new ArrayList<>();
+		result.add(arr[0]);
+		for(int i=1; i<arr.length; i++) {
+			if(arr[i]!=arr[i-1]) {
+				result.add(arr[i]);
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {10,10,20,20,30,40,10};
+		ArrayList<Integer> result = removeconsecutiveDublicates(arr);
+		for(int i=0; i<result.size(); i++) {
+			System.out.println(result.get(i));
+		}
+
+	}
+
+}
