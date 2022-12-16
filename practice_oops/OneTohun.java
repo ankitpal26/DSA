e45868b667a4b8c65c632f994f1e318c5fce861a
Ankit <ankitpal28090016@gmail.com>
2022-12-16 09:25:17 +0530

    final commit

---
+++ practice_oops/OneTohun.java
@@ -0,0 +1,17 @@
+package practice_oops;
+
+public class OneTohun {
+	static void printNum(int n) {
+		if(n>0) {
+			printNum(n-1);
+			System.out.println(n + " ");
+		}
+		return;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       printNum(100);
+	}
+
+}
