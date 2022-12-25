5fedefea6a3a9814e0080f29b09be81625ab0f6c
Ankit <ankitpal28090016@gmail.com>
2022-12-26 00:23:18 +0530

    intial commit

---
+++ stack/StackUse.java
@@ -0,0 +1,21 @@
+package stack;
+
+public class StackUse {
+
+	public static void main(String[] args) throws StackFullException {
+		// TODO Auto-generated method stub
+		StackUsingArray stack = new StackUsingArray();
+		for(int i=1; i<=5; i++) {
+			stack.push(i);
+		}
+		while(!stack.isEmpty()) {
+			try {
+				System.out.println(stack.pop());
+			} catch (StackEmptyException e) {
+				// TODO: handle exception
+			}
+		}
+
+	}
+
+}
--- stack/StackUsingArray.java
+++ stack/StackUsingArray.java
@@ -1,5 +1,57 @@
 package stack;
 
 public class StackUsingArray {
+	private int data[];
+	private int top;
+	
+	public StackUsingArray(int capacity) {
+		
+		data = new int [capacity];
+		top = -1;
+	}
+	public StackUsingArray() {
+		data = new int[10];
+		top = -1;
+		// TODO Auto-generated constructor stub
+	}
+	public  boolean isEmpty() {
+		
+		return (top == -1);
+	}
 
+	public int size() {
+		return top + 1;
+	}
+	public int top() throws StackEmptyException {
+		if(size()== 0) {
+			//stackEmptyException
+			StackEmptyException e = new StackEmptyException();
+			throw e;
+		}
+		return data[top];
+	}
+    
+	public void push(int elem) throws StackFullException {
+
+		if(size() == data.length) {
+			//stack full
+			StackFullException e = new StackFullException();
+			throw e;
+			}
+		top++;
+		data[top]=elem;
+	}
+	
+	public int pop() throws StackEmptyException {
+		if(size() == 0) {
+			//StackEmptyException
+			StackEmptyException e = new StackEmptyException();
+			throw e;
+		}
+		
+		int temp = data[top];
+		top--;
+		return temp;
+	}
 }
+
