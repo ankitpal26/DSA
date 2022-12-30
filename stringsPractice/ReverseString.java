1243b22eed2eace4cad384377997f9d5ceec9d6d
Ankit <ankitpal28090016@gmail.com>
2022-12-30 08:33:51 +0530

    Tree Structure

--- stringsPractice/ReverseString.java
+++ stringsPractice/ReverseString.java
@@ -1,23 +1,20 @@
 package stringsPractice;
 
+import java.util.Scanner;
+
 public class ReverseString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str = "ANKIT";
-		String str2="Pal";
-		String reverse = "";
-		String reverse2="";
+	     Scanner in = new Scanner(System.in);
+	     String str = in.nextLine();
+	     //char[] reverse = str.toCharArray();
 		for(int i=str.length()-1; i>=0; i--) {
-			reverse += str.charAt(i);
-			
-		}
-		for(int j=str.length()-1; j>=0; j--) {
-			reverse2+=str2.charAt(j);
-		}
-		System.out.println(reverse);
-		System.out.println(reverse2);
+//			reverse += str.charAt(i);
+		    System.out.print(str.charAt(i));
+	
 
 	}
 
+	}
 }
