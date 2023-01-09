1b61f5daaaec3457b1a816a30c83ac98ea36640c
Ankit <ankitpal28090016@gmail.com>
2023-01-10 00:02:31 +0530

    intial commit

--- stringsPractice/RemoveDublicates.java
+++ stringsPractice/RemoveDublicates.java
@@ -1,5 +1,21 @@
 package stringsPractice;
 
 public class RemoveDublicates {
+	public static String removeConsecutiveDublicates(String s) {
+		if(s.length() <= 1) {
+			return s;
+		}
+		if(s.charAt(0) == s.charAt(1)) {
+			return removeConsecutiveDublicates(s.substring(1));
+		}
+		else {
+			String small = removeConsecutiveDublicates(s.substring(1));
+			return s.charAt(0)+small;
+		}
+	}
+	public static void main(String[] args) {
+		String str="aabbccddee";
+		System.out.println(removeConsecutiveDublicates(str));
+	}
 
 }
