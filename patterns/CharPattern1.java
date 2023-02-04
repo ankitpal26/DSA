aa5a708aef2e987df9b35b6cf898897bf65168b8
Ankit <ankitpal28090016@gmail.com>
2023-02-04 23:22:34 +0530

    final commit

---
+++ patterns/CharPattern1.java
@@ -0,0 +1,25 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class CharPattern1 {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int i = 1; 
+		{
+			while(i <= n) {
+				int j = 1; 
+				while(j <= i) {
+					System.out.print((char)('A' + j - 1 )+ " ");
+					j++;
+				}
+				System.out.println();
+				i++;
+			}
+		}
+	}
+
+}
