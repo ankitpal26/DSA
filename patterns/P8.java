d64ff6ab3ef71b1d5ed3e494c02343cb154bc3ac
Ankit <ankitpal28090016@gmail.com>
2023-02-04 23:08:44 +0530

    final commit

---
+++ patterns/P8.java
@@ -0,0 +1,28 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class P8 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int i = 1; 
+		int counter = 1;
+		{
+			while(i <= n) {
+				int j = 1;
+				
+				while(j <= i) {
+					System.out.print(counter+ " ");
+					counter++;
+					j++;
+				}
+				System.out.println();
+				i++;
+			}
+		}
+
+	}
+
+}
