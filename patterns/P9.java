c2e3c5715a5216a05e16bf6b70c5e725443a67ad
Ankit <ankitpal28090016@gmail.com>
2023-02-06 23:41:39 +0530

    intial commit

---
+++ patterns/P9.java
@@ -0,0 +1,37 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class P9 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int i = 1; 
+		{
+			while(i <= n) {
+				int j = 1;
+				
+				while(j <= n-i) {
+					System.out.print(" ");
+					j++;
+				}
+				int num=1;
+				while(num <= i) {
+					System.out.print("*");
+					num++;
+				}
+				int num2=1;
+				while(num2 <= i-1) {
+					System.out.print("*");
+					num2++;
+				}
+				System.out.println();
+				i++;
+			}
+		}
+
+
+	}
+
+}
